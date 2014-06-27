e34520dc08d14e2a19d71534a0f69739cf15f60e
Ole Kröger <o.kroeger@wikunia.de>
2014-06-27 11:36:35 +0200

    part can be part of dir

--- bg.js
+++ bg.js
@@ -7,37 +7,49 @@ chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
 	// generate an array with all regular expressions and tupel of a filename part and a directory
 	dirArray = [];
 	dirArray[0] = [];
+	dirArray[1] = [];
 	// url with regex
 	dirArray[0]["urlRegEx"] = /https:\/\/studip.tu-clausthal.de\/sendfile.php?(.*?)&file_name=(.*?).pdf/;
 	dirArray[0]["RegExNr"] = 2; // use the second part of the regex (after file_name)
 	// create the (part of filename,dir) tupel
 	dirArray[0]["indexDir"] = [
-								{part:"Programmierkurs",dir:"S2/Programmierkurs/"},
-								{part:"Ana2",dir:"S2/Analysis/"},
-							  	{part:"IN2",dir:"S2/Informatik/"},
-								{part:"la2",dir:"S2/LADS/"},
-								{part:"Haus%FCbung",dir:"S2/LADS/HA/"},
-								{part:"Tutor%FCbung",dir:"S2/LADS/Übung/"}
+								{part:"Programmierkurs",dir:"Documents/CLZ/TUC/S2/Programmierkurs/"},
+								{part:"Ana2",dir:"Documents/CLZ/TUC/S2/Analysis/"},
+							  	{part:"IN2",dir:"Documents/CLZ/TUC/S2/Informatik/"},
+								{part:"la2",dir:"Documents/CLZ/TUC/S2/LADS/"},
+								{part:"Haus%FCbung",dir:"Documents/CLZ/TUC/S2/LADS/HA/"},
+								{part:"Tutor%FCbung",dir:"Documents/CLZ/TUC/S2/LADS/Übung/"}
 							  ];
+	dirArray[1]["urlRegEx"] = /http:\/\/visualizing.org\/sites\/default\/files\/data_set\/(.*)\/(.*?)/;
+	dirArray[1]["RegExNr"] = 1; // use the second part of the regex (after file_name)
+	dirArray[1]["indexDir"] = [
+								{part:"",dir:"Documents/Visualizing/!part!/"},
+							  ];
+
 	// dir is default directory
-	var dir = "";
+	var dir = "Downloads/";
 	var break_out = false;
 	// iterate through all regexes
 	for (var r = 0; r < dirArray.length; r++) {
 		// if regex found
-		if (regex = dirArray[0]["urlRegEx"].exec(item.url)) {
+		if (regex = dirArray[r]["urlRegEx"].exec(item.url)) {
 			// get the right part out of the regex array
-			var test_var = regex[dirArray[0]["RegExNr"]];
+			var test_var = regex[dirArray[r]["RegExNr"]];
 			/*
-			console.log(dirArray[0]["indexDir"].length);
-			console.log(dirArray[0]["indexDir"][0]);
+			console.log(dirArray[r]["indexDir"].length);
+			console.log(dirArray[r]["indexDir"][0]);
 			console.log(test_var);
 			*/
 			// iterate through all filename,dir tupel
-			for (var i = 0; i < dirArray[0]["indexDir"].length; i++) {
+			for (var i = 0; i < dirArray[r]["indexDir"].length; i++) {
+				if (dirArray[r]["indexDir"][i].part == "") {
+						dirArray[r]["indexDir"][i].part = test_var;
+				}
+				dirArray[r]["indexDir"][i].dir = urldecode(dirArray[r]["indexDir"][i].dir.replace('!part!',dirArray[r]["indexDir"][i].part));
+
 				// if current tupel is the correct one (part is a part of the filename)
-				if (test_var.indexOf(dirArray[0]["indexDir"][i].part) != -1) {
-					dir = dirArray[0]["indexDir"][i].dir;
+				if (test_var.indexOf(dirArray[r]["indexDir"][i].part) != -1) {
+					dir = dirArray[r]["indexDir"][i].dir;
 					break_out = true;
 					break;
 				}
@@ -57,3 +69,8 @@ chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
 		   conflictAction: 'overwrite'});
  
 });
+
+
+function urldecode(url) {
+  return decodeURIComponent(url.replace(/\+/g, ' '));
+}
